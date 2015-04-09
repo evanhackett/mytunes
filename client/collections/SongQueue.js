2fdb9c0e144b7b4982113c0d5b76a0a5453b2653
evanhackett <evanwhackett@gmail.com>
2015-04-08 20:04:47 -0700

    All back end tests working. Views still need work

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,29 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
-  }
+
+    this.on('add', function(event) {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function() {
+      this.shift();
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function(model) {
+      this.remove(model);
+    }, this);
+
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
 
 });
